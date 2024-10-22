9213bf51a1d35054c148b887cae3eaca196f8b62
BinhNguyen231 <binhnn2301@gmail.com>
2024-10-22 23:20:04 +0700

    22-10

--- resize_image.py
+++ resize_image.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 
-p_raw = "images/gallery_raw"
+p_raw = "..\\..\\anh_da_chinh_sua\\anh_le"
 for image in os.listdir(p_raw):
     img = cv2.imread(os.path.join(p_raw, image))
     h,w,_ = img.shape
@@ -15,5 +15,5 @@ for image in os.listdir(p_raw):
         new_w = 1080
         new_h = int(new_w * ratio)
     new_img = cv2.resize(img, (new_w, new_h))
-    cv2.imwrite(os.path.join("images/gallery", image), new_img)
+    cv2.imwrite(os.path.join("..\\..\\anh_da_chinh_sua\\anh_le_resize", image), new_img)
 
